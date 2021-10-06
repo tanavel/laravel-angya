b382d873f66eed62c67cd0d2d959e86cb8e8b551
tanavel <tanavel1118@gmail.com>
2021-10-06 09:44:41 +0900

    投稿機能の作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Hello world.';
-});
+Route::get('/message/create', [MessageController::class, 'create']);
+Route::post('/message/create', [MessageController::class, 'store']);
